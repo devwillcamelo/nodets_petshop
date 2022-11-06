6c64ba28688a4632cd647519b7d943a7ea72435a
William Camelo <devwillcamelo@gmail.com>
2022-11-06 17:47:02 -0300

    finaly project 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto Canil
+Projeto feito no módulo do curso Node + Typescript
+
+
+### Pré-requisitos globais
+`npm i -g nodemon typescript ts-node`
+
+###para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express  from "express";
+import dontev from 'dotenv';
+import mustache  from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dontev.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+// Rotas
+server.use(mainRoutes);
+
+
+server.use((req, res) => {
+    res.render('pages/404');
+});
+
+
+server.listen(process.env.PORT);
